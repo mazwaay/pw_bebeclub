0b04dd46b5a47388187aa9b60a1196d17804ab36
@mazwaay <wahyucheger51@gmail.com>
2025-02-21 14:59:01 +0700

    create a new file

--- telegramReports/run_all.py
+++ telegramReports/run_all.py
@@ -4,12 +4,18 @@ from playwright.async_api import async_playwright
 # Import semua file Playwright
 import login_only_input_password
 import login_only_input_phoneNumber
+import login_password
+import login_with_invalid_phoneNumber
+import login_with_invalid_phoneNumber2
 
 # Fungsi untuk menjalankan setiap file Playwright
 async def run_all():
     async with async_playwright() as p:
         await login_only_input_password.open_bebeclub()
         await login_only_input_phoneNumber.open_bebeclub()
+        await login_password.open_bebeclub()
+        await login_with_invalid_phoneNumber.open_bebeclub()
+        await login_with_invalid_phoneNumber2.open_bebeclub()
 
 if __name__ == "__main__":
     asyncio.run(run_all())
